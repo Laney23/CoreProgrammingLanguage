cdfb06314efdf678174fa062906fa3cf27bf7313
Laney23 <laney.25@osu.edu>
2017-08-17 19:24:09 +0200

    Updated the README to include CORE Abstract Syntax Tree

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-//
-//  main.cpp
-//  CoreProgrammingLanguage
-//
-//  Created by Josh Laney on 8/17/17.
-//  Copyright © 2017 Aginor. All rights reserved.
-//
+/*
+*  main.cpp
+*  CoreProgrammingLanguage
+*
+*  Created by Josh Laney on 8/17/17.
+*  Copyright © 2017 Aginor. All rights reserved.
+*/
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(int argc, const char * argv[])
+{
     std::cout << "Hello, World!\n";
     return 0;
 }

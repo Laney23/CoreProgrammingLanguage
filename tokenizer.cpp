4efcd8a6b44eb90c5cd8c91625b433a657186e84
Laney23 <laney.25@osu.edu>
2017-08-17 20:31:28 +0200

    Initial tokenizer functions

---
+++ tokenizer.cpp
@@ -0,0 +1,73 @@
+//
+//  tokenizer.cpp
+//  CoreProgrammingLanguage
+//
+//  Created by Josh Laney on 8/17/17.
+//  Copyright © 2017 Aginor. All rights reserved.
+//
+
+#include "tokenizer.hpp"
+
+/*
+ * Name: getToken
+ *
+ * Purpose: returns the first token
+ *
+ * Parameters:
+ *
+ * Return:
+ */
+int getToken()
+{
+    return 0;
+} /* getToken */
+
+
+/*
+ * Name: skipToken
+ *
+ * Purpose: skips the first token so that the next token becomes the current token.
+ *
+ * Parameters:
+ *
+ * Return:
+ */
+int skipToken()
+{
+    return 0;
+} /* skipToken */
+
+
+/*
+ * Name: intVal
+ *
+ * Purpose: returns the integer value of the current token.
+ *
+ * Parameters:
+ *
+ * Return:
+ */
+int intVal()
+{
+    return 0;
+} /* intVal */
+
+
+/*
+ * Name: idName
+ *
+ * Purpose: returns the name of the current token.
+ *
+ * Parameters:
+ *
+ * Return:
+ */
+int idName()
+{
+    return 0;
+} /* idName */
+
+
+
+
+
---
+++ tokenizer.hpp
@@ -0,0 +1,19 @@
+//
+//  tokenizer.hpp
+//  CoreProgrammingLanguage
+//
+//  Created by Josh Laney on 8/17/17.
+//  Copyright © 2017 Aginor. All rights reserved.
+//
+
+#ifndef tokenizer_hpp
+#define tokenizer_hpp
+
+#include <stdio.h>
+
+#endif /* tokenizer_hpp */
+
+int getToken();
+int skipToken();
+int intVal();
+int idName();

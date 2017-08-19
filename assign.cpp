a85fe74885585c8aa34ce337848a3a25f11125cd
Laney23 <laney.25@osu.edu>
2017-08-20 00:30:03 +0200

    Added assign class

---
+++ assign.cpp
@@ -0,0 +1,106 @@
+//
+//  assign.cpp
+//  CoreProgrammingLanguage
+//
+//  Created by Josh Laney on 8/19/17.
+//  Copyright © 2017 Aginor. All rights reserved.
+//
+
+#include "assign.hpp"
+
+
+/*
+ * Name: Assign constructor
+ * Purpose: initializes class values
+ * Parameters:  Tokenizer toke                 tokenizer object to parse
+ */
+Assign::Assign()
+{
+    /* Initialize variables */
+    Assign::e = Exp();
+    Assign::id = Id();
+} /* function Assign constructor */
+
+
+/*
+ * Name: parse
+ * Purpose: parse the Assign object
+ * Parameters: Tokenizer t          token object to parse
+ * Return: SUCCESS or ERROR
+ */
+int Assign::parse(Tokenizer t)
+{
+    /* Parse the identifier */
+    if (Assign::id.parse(t) != SUCCESS)
+        return ERROR;
+    
+    /* Remove '=' */
+    TokenPair p = t.getToken();
+    if(p.value != EQUAL)
+    {
+        printf("Expected '=' \n");
+        return ERROR;
+    }
+    
+    /* Parse the expression */
+    if (Assign::e.parse(t) != SUCCESS)
+        return ERROR;
+    
+    /* Remove ';' */
+    p = t.getToken();
+    if(p.value != SEMIC)
+    {
+        printf("Expected ';' \n");
+        return ERROR;
+    }
+    
+    return SUCCESS;
+} /* function parse */
+
+
+/*
+ * Name: execute
+ * Purpose: execute the Assign object
+ * Return: SUCCESS or ERROR
+ */
+int Assign::execute()
+{
+    /* Check if the identifier is already in the table and then update its value if it is */
+    std::string name = Assign::id.getName();
+    if (ParseObject::inTable(name) >= 0)
+    {
+        if (Assign::id.setId(Assign::e.execute()) != SUCCESS)
+            return ERROR;
+    }
+    else
+    {
+        printf("Identifier must be declared in declaration sequence.\n");
+        return ERROR;
+    }
+    
+    return SUCCESS;
+} /* function execute */
+
+
+/*
+ * Name: print
+ * Purpose: print the Iff object
+ * Return: SUCCESS or ERROR
+ */
+int Assign::print()
+{
+    /* Print the identifier */
+    std::string str = Assign::id.getName();
+    str += " = ";
+    printf("%s", str.c_str());
+    
+    /* Print the expression */
+    if (Assign::e.print() != SUCCESS)
+        return ERROR;
+    
+    /* Print ';' */
+    printf(";\n");
+    
+    return SUCCESS;
+} /* function print */
+
---
+++ assign.hpp
@@ -0,0 +1,32 @@
+//
+//  assign.hpp
+//  CoreProgrammingLanguage
+//
+//  Created by Josh Laney on 8/19/17.
+//  Copyright © 2017 Aginor. All rights reserved.
+//
+
+#ifndef assign_hpp
+#define assign_hpp
+
+
+#include <stdio.h>
+#include "parse.hpp"
+#include "tokenizer.hpp"
+#include "exp.hpp"
+#include "id.hpp"
+
+
+class Assign : ParseObject {
+    Exp e;
+    Id id;
+    
+public:
+    Assign();
+    int parse(Tokenizer t);
+    int execute();
+    int print();
+};
+
+
+#endif /* assign_hpp */
